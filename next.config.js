32c12e248978de2bf07526bbf89385e3b5361e0c
Rahul Yadu <rahulyadu17@gmail.com>
2024-09-30 19:23:22 +0530

    next config changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:'export',
-    basePath:'/build'
+    // output:'export',
+    // basePath:'/build'
 }
 
 module.exports = nextConfig
