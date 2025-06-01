c800960cf8ab6ec8b8c0d693c38abe2802ed4bfe
Rahul Yadu <rahulyadu17@gmail.com>
2025-06-01 23:41:47 +0530

    feat: remove dot.tsx file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,46 +1,41 @@
-import type { Metadata } from 'next'
-import { Inter} from 'next/font/google'
-import './globals.css'
-import { ThemeProvider } from '@/components/theme-provider'
-import Dot from '@/components/animata/background/Dot'
-const inter = Inter({ weight: ['400'], subsets: ['latin'] })
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+const inter = Inter({ weight: ["400"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  icons:'/code.png',
-  title: 'Rahul yadu',
-  description: 'Rahul yadu is a software engineer who has experience in Full Stack Web Development (MERN stack) . he has also interest in networking and systems programming',
-  openGraph:{
-    title:"Rahul yadu Software Engineer",
-    description:"Rahul yadu is a software engineer who has experience in Full Stack Web Development (MERN stack) . he has also interest in networking and systems programming",
-    images:["/code.png"]
+  icons: "/code.png",
+  title: "Rahul yadu",
+  description:
+    "Rahul yadu is a software engineer who has experience in Full Stack Web Development (MERN stack) . he has also interest in networking and systems programming",
+  openGraph: {
+    title: "Rahul yadu Software Engineer",
+    description:
+      "Rahul yadu is a software engineer who has experience in Full Stack Web Development (MERN stack) . he has also interest in networking and systems programming",
+    images: ["/code.png"],
   },
-  twitter:{
-    title:"Rahul yadu Software Engineer",
-    card:"summary",
-    description:"Rahul yadu is a software engineer who has experience in Full Stack Web Development (MERN stack) . he has also interest in networking and systems programming",
-    images:["/code.png"]
-  }
-}
+  twitter: {
+    title: "Rahul yadu Software Engineer",
+    card: "summary",
+    description:
+      "Rahul yadu is a software engineer who has experience in Full Stack Web Development (MERN stack) . he has also interest in networking and systems programming",
+    images: ["/code.png"],
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          
-          enableSystem
-        >
-          
-
+        <ThemeProvider attribute="class" enableSystem>
           {children}
-          
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,22 @@
-"use client"
+"use client";
 
-import Header from '@/components/Header'
-import Intro from '@/components/Intro'
-import Projects from '@/components/Projects'
-import Skills from '@/components/Skills'
-import Footer from '@/components/Footer'
-import Dot from '@/components/animata/background/Dot'
-import { useTheme } from 'next-themes'
+import Header from "@/components/Header";
+import Intro from "@/components/Intro";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
+import Footer from "@/components/Footer";
+import { useTheme } from "next-themes";
 export default function Home() {
-  const {theme} = useTheme()
-  console.log(theme)
-  
-  return (
-    <Dot style={{backgroundColor:theme==='dark'?'black':'white'}} spacing={20}>
+  const { theme } = useTheme();
+  console.log(theme);
 
-   <main className='flex flex-col gap-20 items-center justify-center'>
-    <Header />
-    <Intro/>
-    <Projects/>
-    <Skills/>
-    <Footer/>
-   </main> 
-    </Dot>
-  )
+  return (
+    <main className="flex flex-col gap-20 items-center justify-center mx-10">
+      <Header />
+      <Intro />
+      <Projects />
+      <Skills />
+      <Footer />
+    </main>
+  );
 }
